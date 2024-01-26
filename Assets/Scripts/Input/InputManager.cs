e3c75599b1f9c72ef1cf0df4380c9a67588de55b
Amine Salmane <amine.games2002@gmail.com>
2024-01-26 23:59:37 +0100

    jjj

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -34,8 +34,6 @@ public class InputManager : MonoBehaviour
             playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
             playerControls.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
 
-            // Set up mouse click input
-            playerControls.PlayerMovement.Mouse.started += _ => OnMouseClick();
 
             // Enable the controls
             playerControls.Enable();
@@ -60,24 +58,13 @@ public class InputManager : MonoBehaviour
         cameraInputY = cameraInput.y;
     }
 
-    // New method to handle mouse click
-    private void OnMouseClick()
-    {
-        isMouseClick = true;
-        // Handle additional logic for mouse click if needed
-    }
+
 
     public void HandleAllInputs()
     {
         HandleMovementInput();
 
-        // Check if there was a mouse click
-        if (isMouseClick)
-        {
-            // Handle mouse click logic
-            Debug.Log("Mouse Clicked!");
-            isMouseClick = false; // Reset the flag
-        }
+
 
         // Add logic for other inputs (e.g., jump, interact, dash) here
     }
